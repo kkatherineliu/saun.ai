80f13ac08cb98e42a23481183f9c80179e01a6e9
kkathereliu <katherineliu327kl@gmail.com>
2026-02-07 01:48:32 -0500

    added basic shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+const Page = ({ text }: { text: string }) => {
+  return (
+    <div className="flex min-h-screen flex-1 items-center">
+      <p className="text-lg text-foreground">{text}</p>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <p className="text-lg text-foreground">hi insert landing page here</p>
+    <div className="flex min-h-screen flex-row">
+      <Page text="hi insert landing page here" />
     </div>
   );
 }
---
+++ components/ChatSidebar.tsx
@@ -0,0 +1,6 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+// component for display each chat message depending on user vs ai message
+
+// overall sidebar component
