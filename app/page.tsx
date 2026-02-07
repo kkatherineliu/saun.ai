cbeb06040f06d5fa37e59596a93b967b2a04dead
kkathereliu <katherineliu327kl@gmail.com>
2026-02-07 00:23:44 -0500

    initial commit, create next app

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p className="text-lg text-foreground">hi insert landing page here</p>
+    </div>
+  );
+}
