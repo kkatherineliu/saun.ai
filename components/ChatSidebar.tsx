16251cd264ee7ef49a6dd2a291da7ec3d5611438
Clement Zhou <clmementzhou2@gmail.com>
2026-02-07 15:14:55 -0500

    whole lotta nothin ADD two divs to project

--- components/ChatSidebar.tsx
+++ components/ChatSidebar.tsx
@@ -1,6 +1,20 @@
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
+
 // component for display each chat message depending on user vs ai message
 
 // overall sidebar component
+
+export default function ChatSidebar() {
+    return (
+        <div className="flex justify-between items-center gap-2">
+            <div className="bg-blue-800">
+                Text test
+            </div>
+            <div className="bg-black">
+                sidebar
+            </div>
+        </div>
+    )
+}
